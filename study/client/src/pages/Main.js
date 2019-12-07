6d5bb880bebe98f33a63f832f1d9e68cd537cd6c
Zion Brown <zabs95@gmail.com>
2019-12-07 11:46:07 -0500

    Added a node_modules, sorry if the .gitignore doesnt stop it

--- study/client/src/pages/Main.js
+++ study/client/src/pages/Main.js
@@ -1,28 +1,47 @@
 import React, { Component } from "react";
 import { Col, Row, Container } from "../components/Grid";
 import { Input, TextArea, FormBtn } from "../components/Form";
+import API from "../utils/API";
 
 class Main extends Component {
+
+    handleSubmit = event => {
+event.preventDefault()
+
+if (this.state.name && this.state.password) {
+    API.saveBook({
+      name: this.state.title,
+      password: this.state.password,
+    })
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+    }
+};
+    
+
 render() {
 return (
 
 <Container fluid>
     <Row>
-        <Col size="md-6">
+        <Col size="md-3">
 <form>
     Log in:
     <Input
-        //value={this.state.title}
-        //onChange={this.handleInputChange}
+        value={this.state.name}
+        onChange={this.handleInputChange}
         name="name"
         placeholder="enter name here"
         />
         <Input
-        //value={this.state.title}
-        //onChange={this.handleInputChange}
+        value={this.state.password}
+        onChange={this.handleInputChange}
         name="password"
-        placeholder="enter name here"
+        placeholder="enter password here"
         />
+        <FormBtn
+        onClick={this.handleSubmit} //when this button is clicked, it submits the form
+        >Submit</FormBtn>
 </form>
         </Col>
     </Row>
