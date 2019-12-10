8e102d263c0422cc6480c82a0d0878ccd18baab8
Emily Tucker <emilytucker4@yahoo.com>
2019-12-10 11:01:17 -0500

    finished the new user form and deleted the english folder because im not wroking on that

---
+++ study/client/src/components/New/New.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+const New = props =>{
+    return(
+        <form className="login">
+            <div className="ui form">
+                <div className="fields">
+                    <div className="field">
+                        <input 
+                        name="username"
+                        type="text" 
+                        placeholder="Username"
+                        value={value.username}
+                        onChange={handleUserChange}
+                        />
+                    </div>
+                    <div className="field">
+                        <input
+                        name="email"
+                        type="text"
+                        placeholder="Email"
+                        value={value.email}
+                        onChange={handleEmailChange}
+                        />
+                    </div>
+                    <div className="field">
+                        <input 
+                        name="password"
+                        type="password"
+                        placeholder="Password"
+                        value={value.password}
+                        onChange={handlePassChange}
+                        />
+                    </div>
+                </div>
+            </div>
+        </form>
+    )
+};
+
+export default New;
+
+
+//<input
+    // name="email"
+    // type="text"
+    // placeholder="Email"
+    // value={values.email}
+    // onChange={handleChange}
+    // name="password"
+    // type="password"
+    // placeholder="Password"
+    // value={value.password}
+    // onChange={handleChange}
+    // />
+
+
