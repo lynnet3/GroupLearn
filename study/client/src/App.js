2dd090877789712bb745cde47abb3516c5bdcf90
Zion Brown <49786645+LardianGH@users.noreply.github.com>
2019-12-18 13:36:12 -0500

    Wynton merge (#16)
    * login.html, working on homepage
    
    * Login.js & NavBar.js componenrs
    
    * Delete package.json
    
    * editing index.html
    
    * reformatting index.html
    
    * Grid & List component
    
    * npm installed
    
    * Sorry wynton
    
    * stuff
    
    * Can retrieve username on frontend frontpage
    
    * Seperated login and signup forms, routing works

--- study/client/src/App.js
+++ study/client/src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import Main from "./pages/Main";
+import FrontPage from "./pages/FrontPage";
+import Signup from "./pages/Signup"
+import Login from "./pages/Login"
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
-
 function App() {
   return (
     <Router>
       <div>
         <Nav />
         <Switch>
-          <Route exact path="/" component={Main}/>
-          <Route exact path="/user/:id" component={Main} />
+          <Route exact path="/" component={FrontPage}/>
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/login" component={Login} />
           <Route exact path="/subjects/:id" component={Main} />
           <Route component={NoMatch} />
         </Switch>
@@ -19,5 +22,4 @@ function App() {
     </Router>
   );
 }
-
 export default App;
---
+++ study/client/src/pages/Signup.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { Container } from "../components/Grid";
+import  Form2  from "../components/Form2/index.js";
+import  LogInForm  from "../components/LogInForm";
+import {FormBtn} from "../components/Form"
+import API from "../utils/API";
+import PostGroup from "../components/PostGroup";
+
+class Signup extends Component { //main is a test page to test user creation (Working)
+
+//something is wrong
+render() {
+  
+return (
+
+<Container fluid>
+
+    <Form2>
+    </Form2>
+
+</Container>
+
+)
+}
+}
+
+export default Signup;
