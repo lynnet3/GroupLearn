a061099376ce5b69b90bffb21bf2437c10ce2921
Jim Chung <jchung225@gmail.com>
2019-12-18 19:10:19 -0500

    created the chatroom.js page

--- study/client/src/pages/Chatroom.js
+++ study/client/src/pages/Chatroom.js
@@ -1,1 +1,20 @@
 import React, { Component } from "react";
+import Nav, {Component} from "../components/Nav/navBar 2";
+import submit, {Component} from "./components/Submit Button/index";
+import Cancel, {component} from "./components/Cancel Button/index";
+import MessageBoard, {component} from "./components/MessageBoard/index";
+import ChatPopup from "./components/ChatPopUp/index";
+import Jumbotron from "./components/Jumbotron/index";
+
+class Chatroom extends Component {
+
+    render(){
+        return (
+            <div className= "container">
+            <navBar/>
+            <Header/>
+            </div>
+        
+        );
+    }
+}
---
+++ study/client/src/pages/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div id="header-content">
+        <header>
+          <h1>Math Group</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Header;
