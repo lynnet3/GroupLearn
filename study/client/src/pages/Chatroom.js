07333369d11ba19c5d6e0706020c83eb00efdc98
Jim Chung <jchung225@gmail.com>
2019-12-21 10:03:07 -0500

    submit button linked

--- study/client/src/pages/Chatroom.js
+++ study/client/src/pages/Chatroom.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Nav from "../components/Nav/navBar 2";
-import submit from "./components/Submit Button/index";
+import Submit from "./components/Submit Button/index";
 import Cancel from "./components/Cancel Button/index";
 import MessageBoard from "./components/MessageBoard/index";
 import ChatPopup from "./components/ChatPopUp/index";
