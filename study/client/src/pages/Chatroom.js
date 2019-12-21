0c1a37497c94a4365f0110571a0c21f34706c0ce
Jim Chung <jchung225@gmail.com>
2019-12-21 09:51:48 -0500

    updated chatroom.js

--- study/client/src/pages/Chatroom.js
+++ study/client/src/pages/Chatroom.js
@@ -1,18 +1,31 @@
-import React, { Component } from "react";
-import Nav, {Component} from "../components/Nav/navBar 2";
-import submit, {Component} from "./components/Submit Button/index";
-import Cancel, {component} from "./components/Cancel Button/index";
-import MessageBoard, {component} from "./components/MessageBoard/index";
+import React from "react";
+import Nav from "../components/Nav/navBar 2";
+import submit from "./components/Submit Button/index";
+import Cancel from "./components/Cancel Button/index";
+import MessageBoard from "./components/MessageBoard/index";
 import ChatPopup from "./components/ChatPopUp/index";
 import Jumbotron from "./components/Jumbotron/index";
+import Header from "./components/Header/index";
 
 class Chatroom extends Component {
 
     render(){
         return (
             <div className= "container">
-            <navBar/>
+            <Nav/>
             <Header/>
+            <Jumbotron>
+            </Jumbotron>
+            <MessageBoard>
+            </MessageBoard>
+            <ChatPopup>
+            <Submit>
+           <Cancel>
+         </Cancel>
+            </Submit>
+            </ChatPopup>
+
+
             </div>
         
         );
