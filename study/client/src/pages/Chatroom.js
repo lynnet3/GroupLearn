08a14d38645e5db901b6d6aa6add1320810acedf
Jim Chung <jchung225@gmail.com>
2020-01-04 10:28:16 -0500

    merge

--- study/client/src/pages/Chatroom.js
+++ study/client/src/pages/Chatroom.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import Nav from "../components/Nav/navBar 2";
-import Submit from "./components/Submit Button/index";
-import Cancel from "./components/Cancel Button/index";
-import MessageBoard from "./components/MessageBoard/index";
-import ChatPopup from "./components/ChatPopUp/index";
-import Jumbotron from "./components/Jumbotron/index";
-import Header from "./components/Header/index";
-
-class Chatroom extends Component {
-
-=======
 import React, {Component} from "react";
 import { Container } from "../components/Grid";
 import Nav from "../components/Nav/navBar 2";
@@ -57,7 +44,6 @@ data: { data: [] }
         this.getAllPosts()
       }
 
->>>>>>> 2f0a9d7e88274459b1ae7929cc0329b9ab904a20
     render(){
         return (
             <Container fluid>
