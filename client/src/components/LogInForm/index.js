958387493a465c68aa7857ae07f6686cbe757536
Emily Tucker <52586596+lynnet3@users.noreply.github.com>
2020-01-04 12:39:57 -0500

    Emilys branch (#25)
    * moved two of the package and ran a npm i and got gulp
    
    * added the start of the the pages and login form
    
    *  finished the new user form and deleted the english folder because im not wroking on that
    
    * added a file for returning users to put in their username and password
    
    * reworked the returning users page to be be able to be redirect people to the new users page
    
    * added a button to the new users page
    
    * added the icons and changed the new user button color
    
    * made changes so i can do a pull
    
    *  started combining the login forms into one page
    
    * finished the login page form
    
    * finished the login form added it to the current log in form that was there also added the onClick for the buttons
    
    *  made the sign in and sign up two different files again
    
    * changed the look of the returning form
    
    * started making the changes for the returning.js
    
    * finished adding the changes for the returning.js

---
+++ client/src/components/LogInForm/index.js
@@ -0,0 +1,96 @@
+import React, { Component } from "react";
+import { Col, Row, Container } from "../Grid";
+import { Input, TextArea, FormBtn } from "../Form";
+import Returning from "../Returning";
+import Container from "../Container";
+
+class LogInForm extends Component { //main is a test page to test user creation (Working)
+
+    state = { //All the variables that will change
+      userName: "",
+        password: "",
+      };
+
+      handleSubmit = event => { //whenever the form is submitted
+        event.preventDefault()
+      }
+
+handleInputChange = event => { //Allows the textboxes to be used.
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+  
+render() {
+return (
+
+//     <Row>
+//         <Col size="md-3">
+// <form>
+//     Log in:
+//     <Input
+//         value={this.state.name}
+//         onChange={this.handleInputChange}
+//         name="userName"
+//         placeholder="enter userName here"
+//         />
+//         <Input
+//         value={this.state.password}
+//         onChange={this.handleInputChange}
+//         name="password"
+//         placeholder="enter password here"
+//         />
+//         <FormBtn
+//         onClick={this.handleSubmit} //when this button is clicked, it submits the form
+//         >Submit</FormBtn>
+// </form>
+//         </Col>
+//     </Row>
+
+<div>
+<Container style={{ minHeight: "80% "}}>
+  <h3 className="loginText">Welcome new user sign up or sign in</h3>
+  <Returning >
+    handleReturnSubmit={this.handleReturnSubmit}
+    handleUserChange={this.handleUserChange}
+    handlePassChange={this.handlePassChange}
+    
+    handleNewSubmit={this.handleNewSubmit}
+    handleNewUser={this.handleNewUser}
+    handleNewEmail={this.handleNewEmail}
+    handleNewPass={this.handleNewPass}
+  </Returning>
+</Container>
+</div>
+//     <Row>
+//         <Col size="md-3">
+// <form>
+//     Log in:
+//     <Input
+//         value={this.state.name}
+//         onChange={this.handleInputChange}
+//         name="userName"
+//         placeholder="enter userName here"
+//         />
+//         <Input
+//         value={this.state.password}
+//         onChange={this.handleInputChange}
+//         name="password"
+//         placeholder="enter password here"
+//         />
+//         <FormBtn
+//         onClick={e => this.props.handleSubmit(e, this.state.userName, this.state.password)} //when this button is clicked, it submits the form
+//         >Submit</FormBtn>
+// </form>
+
+  {this.state.returnedName}
+    //     </Col>
+    // </Row> //returnedName is from the response
+
+
+}
+
+}
+
+export default LogInForm;
