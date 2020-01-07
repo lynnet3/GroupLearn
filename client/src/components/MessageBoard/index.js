f7acc2cbf099e88936639876c2e10c5e3fa0956a
Zion Brown <49786645+LardianGH@users.noreply.github.com>
2020-01-07 16:09:24 -0500

    Basic features (#31)
    * Individual chatrooms pertaining to the subject ath the end of the route path
    
    * Posts only appear in rooms they were created in.
    
    * moved all files up
    
    * fixed some mess-ups
    
    * last
    
    * remove
    
    * deleted yarn.lock
    
    * Links
    
    * Fixed the css
    
    * made progress towards reply funtionality, added style to chatroom form.

--- client/src/components/MessageBoard/index.js
+++ client/src/components/MessageBoard/index.js
@@ -3,27 +3,68 @@ import Post from "../Post";
 import "./style.css"
 class MessageBoard extends React.Component {
 
+  state = {
+    reply: ""
+  }
+
+  handleInputChange = event => { //Allows the textboxes to be used.
+    const { name, value } = event.target;
+    console.log(name)
+    this.setState({
+      [name]: value
+    });
+  };
+
+  submitReply = (event, data) => { //whenever the form is submitted
+    event.preventDefault()
+    console.log(this.state[data])
+
+   /* if (data) { //if all 2 areas are filled out, run this
+        console.log("replied") //log them
+        API.submitReply({data})
+          .then(res => {
+            console.log(res.data)
+            this.setState({
+              returnedName: res.data.dbModel[0].userName,
+              returnedEmail: res.data.dbModel[0].email
+            });
+            this.props.history.push("/") 
+          })
+          .catch(err => console.log(err));
+        } */
+    };
+
+
   render() {
-    console.log("hiyah friend")
-    if (this.props.data.data) {
-    console.log(this.props.data.data)
-  
+    if (this.props.data.data.length > 0) {
     return (
       <div className="MessageBoard">
         {this.props.data.data.map(post => (
         <Post
         key={post._id}
+        asdf={post._id}
          name = {post.user.userName}
          subject = {post.subject}
          email = {post.user.email}
          location = {post.location}
          content = {post.content}
+         submitReply = {this.submitReply}
+         handleInputChange = {this.handleInputChange}
+         reply = {this.state.reply[post._id]}
         >
         </Post> 
         ))}
       </div> 
     );
-        }
+  } //if no posts exist dont even try to render Posts
+  else {
+    return (
+      <div className="MessageBoard">
+    <h1
+    style={{textAlign: "center"}}>LOADING...</h1>
+    </div>
+    )
+  }
   }
 }
 export default MessageBoard;
